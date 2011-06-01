b1127bb8f7bf08231fab91c52ad637079f5d8d21
Christian Fobel <christian@fobel.net>
2011-06-01 15:40:42 -0400

    Print attributes.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 #include <cstdlib>
 
 using namespace std;
+using namespace pugi;
 
 int main(int argc, char** argv) {
-    pugi::xml_document doc;
+    xml_document doc;
 
     if(argc != 3) {
         cerr << "Usage: " << argv[0] << " <xml_path> <xpath_query>" << endl;
@@ -19,11 +20,18 @@ int main(int argc, char** argv) {
     }
 
     // Select nodes via compiled query
-    pugi::xpath_query query_remote_tools(argv[2]);
-    pugi::xpath_node_set tools = query_remote_tools.evaluate_node_set(doc);
+    xpath_query query_remote_tools(argv[2]);
+    xpath_node_set tools = query_remote_tools.evaluate_node_set(doc);
 
     for(int i = 0; i < tools.size(); i++) {
-        pugi::xpath_node const &tool = tools[i];
-        tool.node().print(cout);
+        xpath_node const &tool = tools[i];
+        xml_node const &n = tool.node();
+        n.print(cout);
+        if(n.attributes_begin() != n.attributes_end()) {
+            cout << "  Attributes:" << endl;
+            for(xml_attribute_iterator iter = n.attributes_begin(); iter != n.attributes_end(); iter++) {
+                cout << "    " << iter->name() << "=\"" << iter->value() << "\"" << endl;
+            }
+        }
     }
 }
